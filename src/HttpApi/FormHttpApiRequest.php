4d8608c96dbb2a14bd51a923375bdba2d979ecb9
jxtou <jxtou@xirelogy.com>
2023-02-03 20:59:06 +0800

    Add FormHttpApiRequest

---
+++ src/HttpApi/FormHttpApiRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace MagpieLib\Interops\HttpApi;
+
+use Magpie\Exceptions\SafetyCommonException;
+use Magpie\Facades\Http\Bodies\HttpFormClientRequestBody;
+use Magpie\Facades\Http\HttpClient;
+use Magpie\Facades\Http\HttpClientPendingRequest;
+use Magpie\Facades\Http\HttpClientResponse;
+use Magpie\General\Names\CommonHttpMethod;
+use Magpie\Objects\UriBuilder;
+use MagpieLib\Interops\Exceptions\CommonInteropsException;
+
+/**
+ * FORM HTTP based API request
+ */
+class FormHttpApiRequest extends CommonHttpApiRequest
+{
+    /**
+     * Perform a POST request with FORM payload
+     * @param UriBuilder|string $url
+     * @param array|HttpFormClientRequestBody $payload
+     * @param iterable $headers
+     * @param iterable $options
+     * @return HttpClientResponse
+     * @throws SafetyCommonException
+     * @throws CommonInteropsException
+     */
+    public function post(UriBuilder|string $url, array|HttpFormClientRequestBody $payload, iterable $headers = [], iterable $options = []) : HttpClientResponse
+    {
+        $urlBuilder = $url instanceof UriBuilder ? $url : UriBuilder::parse($url);
+
+        $requestMakerFn = function () use ($urlBuilder, $payload) : HttpClientPendingRequest {
+            $body = $payload instanceof HttpFormClientRequestBody ? $payload : new HttpFormClientRequestBody($payload);
+            return HttpClient::initialize()->prepare(CommonHttpMethod::POST, $urlBuilder)->withBody($body);
+        };
+
+        return $this->runRequest($requestMakerFn, $headers, $options);
+    }
+}
